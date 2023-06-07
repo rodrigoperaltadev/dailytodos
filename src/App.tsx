5fd6535f35e3ee64dc6d51c42d0ded4996692319
Rodrigo Peralta <rodrigo.peralta@vatech.com>
2023-06-06 22:19:03 -0300

    test husky

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import AppNavigator from './app/navigation/AppNavigator'
+import { ThemeProvider } from 'styled-components/native'
+import { initializeApp } from 'firebase/app'
+import { firebaseConfig } from './infrastructure/firebase/config'
+import { theme } from './presentation/utils/theme/theme'
+
+const algo = 9
+
+// Inicialización de la aplicación de Firebase
+initializeApp(firebaseConfig)
+
+const App = () => {
+  return (
+    <ThemeProvider theme={theme}>
+      <AppNavigator />
+    </ThemeProvider>
+  )
+}
+
+export default App
